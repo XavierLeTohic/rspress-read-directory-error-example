989e5df696459b039cfe64ec49c2d3c956a2df21
Xavier LE TOHIC <xavier.letohic@adevinta.com>
2024-12-19 14:51:08 +0100

    Add rspress config

---
+++ rspress.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+    root: 'docs',
+    title: 'Rspress reading directory',
+    description: 'Example of rspress trying to read directory error',
+});
